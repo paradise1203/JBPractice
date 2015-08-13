7fffb429081cc478f9c1573b123f3dead4d40843
Shayfutdinov Aidar <exoooy@gmail.com>
2015-08-13 21:15:14 +0300

    SpringMVC example - finally, working version

--- #10/SpringMVC/src/main/java/com/aidar/PageController.java
+++ #10/SpringMVC/src/main/java/com/aidar/PageController.java
@@ -1,8 +1,10 @@
 package com.aidar;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/additionForm")
@@ -11,6 +13,13 @@ public class PageController {
     @RequestMapping(method = RequestMethod.GET)
     public String showHomePage() {
         return "additionForm";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String showResult(@RequestParam("firstNumber") String fN, @RequestParam("secondNumber") String sN, ModelMap model) {
+        model.addAttribute("firstNumber", Integer.parseInt(fN));
+        model.addAttribute("secondNumber", Integer.parseInt(sN));
+        return "additionResult";
     }
 
 }
--- #10/SpringMVC/web/WEB-INF/additionResult.jsp
+++ #10/SpringMVC/web/WEB-INF/additionResult.jsp
@@ -1,8 +1,9 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%--
   Created by IntelliJ IDEA.
   User: Aidar_2
   Date: 13.08.2015
-  Time: 19:01
+  Time: 20:45
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
@@ -11,12 +12,6 @@
     <title></title>
 </head>
 <body>
-
-    <form action="/additionForm" method="post">
-        <input type="text" name="firstNumber">
-        <input type="text" name="secondNumber">
-        <input type="submit" value="Summarize">
-    </form>
-
+    <c:out value="${firstNumber+secondNumber}" />
 </body>
 </html>
